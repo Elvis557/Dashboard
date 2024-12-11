ce9fcc86374f37bb32e361d0fd3b364fc4c7d8bb
Elvis <elvisboateng557@gmail.com>
2024-12-11 02:00:51 +0000

    update

---
+++ dash2.js
@@ -0,0 +1,22 @@
+const Orders = [
+    {
+        productName: "Javascript Tutorial",
+        poductNumber: "85743",
+        paymentStatus: "Due",
+        status: "Pending"
+    },
+
+    {
+        productName: "CSS Full Course",
+        poductNumber: "97245",
+        paymentStatus: "Refunded",
+        status: "Pending"
+    },
+
+    {
+        productName: "Flex-Box Tutorial",
+        poductNumber: "36452",
+        paymentStatus: "Paid",
+        status: "Pending"
+    },
+] 
